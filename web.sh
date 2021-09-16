6ff4f6fb95860a65c8e41758b8ba005a640e3088
Chisom Jude <47423151+ChisomJude@users.noreply.github.com>
2021-09-16 22:08:29 +0100

    Update web.sh

--- web.sh
+++ web.sh
@@ -2,5 +2,6 @@
 echo 'Starting Provision: web'$1
  sudo apt-get update
  sudo apt-get install -y nginx
- echo "<h1>Machine: web"$1 "</h1>" > /var/www/index.py
+ sudu apt-get install python3 python3-pip
+ echo "<h1>Machine-web"$1 "</h1>" > /var/www/index.py
  echo 'Provision web'$1 'complete'
